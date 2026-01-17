32d1034f5a8b0d0df51e54efdd93b6f6b9250a72
Nguyễn Tuấn Việt <viet.nguyen@sungrove.co.jp>
2026-01-17 21:40:35 +0700

    Deploy website to GitHub Pages

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/vietnguyen/DATA/PRIVATE/brewkits.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/vietnguyen/DATA/PRIVATE/brewkits.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/vietnguyen/DATA/PRIVATE/brewkits.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/vietnguyen/DATA/PRIVATE/brewkits.github.io/src/css/custom.css"),
+];
